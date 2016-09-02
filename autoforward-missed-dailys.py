d36b5c5cb35382474cf2715db4f0650eaf56df2e
Rick Kasten <rkasten@transperfect.com>
2016-09-01 21:20:13 -0400

    Migrated to python time module, more correct check for yesterday's daily completion

--- autoforward-missed-dailys.py
+++ autoforward-missed-dailys.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 
-import os, requests, subprocess
+import json, os, requests, subprocess, time
 #import pdb; pdb.set_trace()
-
-
-# Return True if there was a history event yesterday (the task was checked)
-def bHistoryYesterday(task):
-    if not task['history']:
-        return False
-    else:
-        # We only want the first 10 characters of the epoch date string
-        latest_history = int(str(task['history'][-1]['date'])[0:10])
-        # latest_history = 1471683605
-        latest_history_date = int(unicode(subprocess.Popen(["date", "-I", "--date='{}'".format(latest_history)], stdout=subprocess.PIPE).communicate()[0].strip(), "utf-8"))
-        return latest_history_date == duedate
-
 
 USR = os.getenv('HAB_API_USER', "YOUR_USERID_HERE")
 KEY = os.getenv('HAB_API_TOKEN', "YOUR_KEY_HERE")
@@ -23,40 +10,38 @@ TXT = "AUTO-FORWARDED"
 
 headers = {"x-api-user":USR,"x-api-key":KEY,"Content-Type":"application/json"}
 
-# Get autoforward tag
+# Get autoforward tag id
 tagsurl = "https://habitica.com/api/v3/tags"
 req = requests.get(tagsurl, headers=headers)
 for tag in (x for x in req.json()['data'] if x['name'] == TAG):
     TAGID = tag['id']
 
-# Abort if there are no dailys with the TAG tag
+# Abort if autoforward tag not found
 try:
     TAGID
 except NameError:
-    print "Autoforward tag \"{}\" not found".format(TAG)
+    # print "Autoforward tag \"{}\" not found".format(TAG)
     quit()
 
 # Abort if you are resting at the inn
 userurl = "https://habitica.com/api/v3/user"
 req = requests.get(userurl, headers=headers)
-if json.dumps(req.json()['data']['preferences']['sleep']):
-    print "Resting in the Inn, autoforwarding cancelled"
+if req.json()['data']['preferences']['sleep']:
+    # print "Resting in the Inn, autoforwarding cancelled"
     quit()
 
-duedate = unicode(subprocess.Popen(["date", "-I", "--date=yesterday"], stdout=subprocess.PIPE).communicate()[0].strip(), "utf-8")
-# day of week (0..6); 0 is Sunday
-daynum = int(unicode(subprocess.Popen(["date", "+%w"], stdout=subprocess.PIPE).communicate()[0].strip(), "utf-8"))
 days = ("su", "m", "t", "w", "th", "f", "s")
-
+# day of week (0..6); 0 is Sunday
+daynum = int(time.strftime("%w"))
 today = days[daynum]
-dueday = days[daynum-1]
+yesterday = days[daynum-1]
 
 # Get dailys
 tasksurl = "https://habitica.com/api/v3/tasks/user?type=dailys"
 req = requests.get(tasksurl, headers=headers)
 
-for daily in req.json()['data']:
-    if daily['repeat'][dueday] and TAGID in daily['tags'] and not bHistoryYesterday(daily):
+for daily in (x for x in req.json()['data'] if TAGID in x['tags']):
+    if daily['repeat'][yesterday] and daily['streak'] == 0:
         daily['repeat'][today] = True
         daily['text'] += " {}".format(TXT)
         taskurl = "https://habitica.com/api/v3/tasks/" + daily['id']
