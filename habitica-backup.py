13e287bc7f34879cc8faa83f39a49bbf622e3165
Rick Kasten <rkasten@transperfect.com>
2016-09-01 21:21:54 -0400

    Migrated to python time module

--- habitica-backup.py
+++ habitica-backup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import json, os, requests, subprocess
+import json, os, requests, subprocess, time
 #import pdb; pdb.set_trace()
 
 USR = os.getenv('HAB_API_USER', "YOUR_USERID_HERE")
@@ -10,8 +10,7 @@ KEY = os.getenv('HAB_API_TOKEN', "YOUR_KEY_HERE")
 hab_url = "https://habitica.com/export/userdata.json"
 headers = {"x-api-key":KEY,"x-api-user":USR,"Content-Type":"application/json"}
 HDB = "location_of_json_db"
-TDB = "location_of_backup_json_db"
-now = unicode(subprocess.Popen(["date", "-Iseconds"], stdout=subprocess.PIPE).communicate()[0].strip(), "utf-8")
+now = unicode(time.strftime("%Y-%m-%dT%H:%M:%S%z"))
 
 # MAIN
 # Load DB
@@ -27,6 +26,5 @@ new[now] = req.json()
 DB.append(new)
 
 # Save DB
-#with open(TDB, 'w') as f:
 with open(HDB, 'w') as f:
     json.dump(DB,f,separators=(',',':'),sort_keys=True)
