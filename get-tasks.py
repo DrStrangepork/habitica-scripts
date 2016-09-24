59bffa9d1e64e52648e8b8fdb2a3674c3b15e8ef
Rick Kasten <rickkasten@gmail.com>
2016-09-24 17:08:38 -0400

    Added get-tasks.py

--- get-tasks.py
+++ get-tasks.py
@@ -8,6 +8,6 @@ KEY = os.getenv('HAB_API_TOKEN', "YOUR_KEY_HERE")
 headers = {"x-api-key":KEY,"x-api-user":USR,"Content-Type":"application/json"}
 
 # MAIN
-req = requests.get("https://habitica.com/api/v3/user", headers=headers)
-with open('user-data.json', 'w') as f:
+req = requests.get("https://habitica.com/api/v3/tasks/user", headers=headers)
+with open('hab_tasks.json', 'w') as f:
     json.dump(req.json(),f,separators=(',',':'),sort_keys=True)
