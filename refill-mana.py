0adeeb85ddbaafb8dcb6c35bdfd067148b38b95f
DrStrangepork <rickkasten@gmail.com>
2022-11-07 08:11:00 -0500

    added re.match for creds

--- refill-mana.py
+++ refill-mana.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import os
+import re
 import sys
 import requests
 
@@ -42,6 +43,9 @@ if args.mp <= 0 or args.multiplier <= 0:
 try:
     if args.user_id is None:
         args.user_id = os.environ['HAB_API_USER']
+    if not re.match(r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', args.user_id, re.IGNORECASE):
+        print(f"Invalid User ID: '{args.user_id}'")
+        sys.exit(1)
 except KeyError:
     print("User ID must be set by the -u/--user-id option or by setting the environment variable 'HAB_API_USER'")
     sys.exit(1)
@@ -49,6 +53,9 @@ except KeyError:
 try:
     if args.api_token is None:
         args.api_token = os.environ['HAB_API_TOKEN']
+    if not re.match(r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', args.api_token, re.IGNORECASE):
+        print(f"Invalid API Token: '{args.api_token}'")
+        sys.exit(1)
 except KeyError:
     print("API Token must be set by the -k/--api-token option or by setting the environment variable 'HAB_API_TOKEN'")
     sys.exit(1)
